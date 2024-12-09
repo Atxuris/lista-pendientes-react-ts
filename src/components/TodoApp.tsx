4b96f723e27eeae4f141c29d1251ec4f700864cd
Atxuris <getatx@gmail.com>
2024-12-08 21:23:41 -0300

    Cambios Realizados

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,34 +1,35 @@
 import { useState } from "react";
 import { ListaTareas } from "./ListaTareas";
 export const TodoApp = () => {
+  const [nuevaTarea, setNuevaTarea] = useState<string>("");
+  const [listaTareas, setlistaTareas] = useState<string[]>([]);
 
-    const [nuevaTarea, setNuevaTarea] = useState<string>('')
-    const [listaTareas, setlistaTareas] = useState<string[]>([])
-    
-    
-    const handleAddTask = () => {
-        if(nuevaTarea.trim() === '') return
-        setlistaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
-        setNuevaTarea('')
-    }
+  const handleAddTask = () => {
+    if (nuevaTarea.trim() === "") return;
+    setlistaTareas((tareasAnteriores) => [...tareasAnteriores, nuevaTarea]);
+    setNuevaTarea("");
+  };
 
-    const handleBorrarTarea = (index: number) => {
-            setlistaTareas(tareas => tareas.filter((_,i) => i !== index ))
-    }
+  const handleBorrarTarea = (index: number) => {
+    setlistaTareas((tareas) => tareas.filter((_, i) => i !== index));
+  };
 
-    return (
-        <div>
-            <h1>Lista de Tareas</h1>
-            <div>
-                <input 
-                type="text"
-                value={nuevaTarea}
-                onChange={(e) => setNuevaTarea(e.target.value)}
-                placeholder="Nueva Tarea"
-                />
-                <button onClick={handleAddTask}>Agregar Tarea</button>
-            </div>
-        <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} ></ListaTareas>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Lista de Tareas</h1>
+      <div className="flex">
+        <input
+          type="text"
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
+          placeholder="Nueva Tarea"
+        />
+        <button onClick={handleAddTask}>Agregar Tarea</button>
+      </div>
+      <ListaTareas
+        listaTareas={listaTareas}
+        borrarTarea={handleBorrarTarea}
+      ></ListaTareas>
+    </div>
+  );
+};
